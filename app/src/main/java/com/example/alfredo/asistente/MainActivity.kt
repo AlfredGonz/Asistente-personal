7f24c9f8c77fdfa71a4b66323e682240947d6e0b
unknown <manu.amg95@gmail.com>
2018-09-06 18:20:29 -0600

    Entorno grafico del asistente

---
+++ app/src/main/java/com/example/alfredo/asistente/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.alfredo.asistente
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    val accesToken = "91ecd7db081b440aab6943ead5401b72"
+    val REQUEST = 200
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun configAsistente()
+    {
+
+    }
+}
